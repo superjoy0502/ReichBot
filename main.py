8eba7190dd826abb5fa9f87431c9c4f5922cde6f
SuperJoy <superjoy0502@gmail.com>
2021-06-08 22:19:29 -0600

    명령어 테스트

--- main.py
+++ main.py
@@ -1,33 +1,29 @@
+# -*- coding: utf-8 -*-
+
 import discord
 import os
 import time
 import discord.ext
 from discord.utils import get
 from discord.ext import commands, tasks
-from discord.ext.commands import has_permissions,  CheckFailure, check
-#^ basic imports for other features of discord.py and python ^
+from discord.ext.commands import has_permissions, CheckFailure, check
 
-client = discord.Client()
+bot = commands.Bot(command_prefix="//")
 
-client = commands.Bot(command_prefix = '!') #put your own prefix here
 
-@client.event
+@bot.event
 async def on_ready():
-    print("bot online") #will print "bot online" in the console when the bot is online
-    
-    
-@client.command()
+    print("bot online")
+
+
+@bot.command(name="핑")
 async def ping(ctx):
-    await ctx.send("pong!") #simple command so that when you type "!ping" the bot will respond with "pong!"
-
-async def kick(ctx, member : discord.Member):
-    try:
-        await member.kick(reason=None)
-        await ctx.send("kicked "+member.mention) #simple kick command to demonstrate how to get and use member mentions
-    except:
-        await ctx.send("bot does not have the kick members permission!")
+    await ctx.send("퐁")
 
 
-client.run(os.getenv("TOKEN")) #get your bot token and make a file called ".env" then inside the file write TOKEN=put your api key here example in env.txt
-#to keep your bot from shutting down use https://uptimerobot.com then create a https:// monitor and put the link to the website that appewars when you run this repl in the monitor and it will keep your bot alive by pinging the flask server
-#enjoy!
+@bot.command(name="생성")
+async def create(ctx):
+    await ctx.send("국가 생성 완료")
+
+
+bot.run(os.getenv("TOKEN"))
